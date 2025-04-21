f669d941aef3265c328a95a60ce65a6a33680aa6
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-21 11:37:50 -0600

    hero section and cryptid cards

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -17,11 +17,10 @@ watch(theme, () => {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-md bg-codeworks border-bottom border-vue">
+  <nav class="navbar navbar-expand-md bg-dark">
     <div class="container gap-2">
-      <RouterLink :to="{ name: 'Home' }" class="d-flex align-items-center text-light">
-        <img class="navbar-brand" alt="logo" src="/img/cw-logo.png" height="45" />
-        <b class="fs-5">Vue Starter</b>
+      <RouterLink :to="{ name: 'Home' }" class="d-flex align-items-center text-light italiana-font fs-1">
+        <span>Cryptipedia</span>
       </RouterLink>
       <!-- collapse button -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links"
@@ -31,19 +30,10 @@ watch(theme, () => {
       <!-- collapsing menu -->
       <div class="collapse navbar-collapse " id="navbar-links">
         <ul class="navbar-nav">
-          <li>
-            <RouterLink :to="{ name: 'About' }" class="btn text-green selectable">
-              About
-            </RouterLink>
-          </li>
         </ul>
         <!-- LOGIN COMPONENT HERE -->
         <div class="ms-auto">
-          <button class="btn text-light" @click="toggleTheme"
-            :title="`Enable ${theme == 'light' ? 'dark' : 'light'} theme.`">
-            <i v-if="theme == 'dark'" class="mdi mdi-weather-sunny"></i>
-            <i v-if="theme == 'light'" class="mdi mdi-weather-night"></i>
-          </button>
+
         </div>
         <Login />
       </div>
@@ -61,7 +51,6 @@ a {
 }
 
 .navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import CryptidCard from '@/components/CryptidCard.vue';
 import { cryptidsService } from '@/services/CryptidsService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
@@ -23,7 +24,68 @@ async function getCryptids() {
 </script>
 
 <template>
-  <div>{{ cryptids }}</div>
+  <div class="container-fluid hero">
+    <div class="row hero-row">
+      <div class="col-md-8 align-self-center">
+        <div class="text-light ps-5">
+          <h2>Terrestrials</h2>
+          <p>
+            A terrestrial cryptid is a creature that exists on land but has not been “scientifically” proven. These
+            creatures often stem from folklore, mythology, or anecdotal evidence. Unlike aquatic cryptids, like the Loch
+            Ness Monster, terrestrial cryptids inhabit forests, mountains, or other land-based environments.
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4 px-0 align-self-end">
+        <img
+          src="https://assets.petco.com/petco/image/upload/f_auto,q_auto:best/catgrooming-pricing-longhair-bath-cut-2.png"
+          alt="emerald entity" class="img-fluid emerald-entity">
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="italiana-font my-4 position-relative">
+          <div>
+            <span class="text-warning bg-text">CRYPTIDS</span>
+          </div>
+          <h1 class="position-absolute">CRYPTIDS</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div v-for="cryptid in cryptids" :key="cryptid.id" class="col-md-3">
+        <CryptidCard :cryptid="cryptid" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.hero {
+  background-image: url(https://images.unsplash.com/photo-1574558452538-7477c4a40b83?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  background-size: cover;
+
+
+  .hero-row {
+    min-height: 70dvh;
+    background: #ababab;
+    background: linear-gradient(0deg, rgba(171, 171, 171, 0.80) 0%, rgba(19, 17, 20, 1) 100%);
+  }
+}
+
+.emerald-entity {
+  filter: hue-rotate(45deg);
+}
+
+.bg-text {
+  font-size: 6rem;
+  user-select: none;
+}
+
+h1 {
+  top: 3.25rem;
+  left: .4rem;
+}
+</style>
